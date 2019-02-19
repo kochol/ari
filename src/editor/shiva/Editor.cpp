e73ff4d477168240e6f8cff8d1b74da1e221b9ca
kochol <cyrusgp@gmail.com>
2019-02-19 07:02:27 +0100

    Add some editor code.

--- src/editor/shiva/Editor.cpp
+++ src/editor/shiva/Editor.cpp
@@ -1,10 +1,52 @@
-#include "shiva.hpp"
+#include "Editor.hpp"
 
 namespace ari
 {
 	namespace sh
 	{
-		
+		Editor* g_pEditor = nullptr;
+
+		Editor::Editor()
+		{
+			g_pEditor = this;
+		}
+
+		Editor::~Editor()
+		{
+			g_pEditor = nullptr;
+		}
+
+		void Editor::Init()
+		{
+			// Load editor settings
+			EditorSettings::Load();
+
+			// Add Systems
+			m_EditorWorld.AddSystem(&m_GuiSystem);
+			m_EditorWorld.AddSystem(&m_RenderSystem);
+			m_EditorWorld.AddSystem(&m_SceneSystem);
+			//m_EditorWorld.SetUpdateType(ari::World::UpdateType::Sync);
+
+			// Init project browser
+			m_ProjectBrowser.Init(&m_EditorWorld);
+		}
+
+		void Editor::Update(float elasped)
+		{
+			m_EditorWorld.Update(elasped);
+		}
+
+		void Editor::LoadProject(Project* project)
+		{
+			delete m_pCurrentProject;
+			m_pCurrentProject = project;
+
+			EditorSettings::Get().LastProjectPath = project->GetPath().get();
+			EditorSettings::Save();
+
+			m_EditorWindow.Init(&m_EditorWorld);
+		}
+
 	} // sh
 	
 } // ari
